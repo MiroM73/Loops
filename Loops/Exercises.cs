10368b1d8216c939ad1c25bd2cf568fc03b5872d
Miroslav Misik <mirom73@gmail.com>
2019-02-03 00:21:28 +0100

    finished exercise 15

--- Loops/Exercises.cs
+++ Loops/Exercises.cs
@@ -340,10 +340,54 @@ namespace Loops
 
             //15.Write a program that converts a given number from hexadecimal to
             //decimal notation.
+            Console.Write("EX15: Convert hexa number to decimal: ");
+            string consoleInput = Console.ReadLine();
+            //string consoleInput = "345AG";
+            decimal resultHexToDec = 0;
+            for (int i = 0, j = consoleInput.Length - 1; i < consoleInput.Length; i++, j--)
+            {
+                if (int.TryParse(consoleInput.Substring(i, 1), out int tmpNumber))
+                {
+                    resultHexToDec += tmpNumber * (decimal)Math.Pow(16,j);
+                }
+                else
+                {
+                    switch (consoleInput.Substring(i,1))
+                    {
+                        case "A":
+                            resultHexToDec += 10 * (decimal)Math.Pow(16, j);
+                            break;
+                        case "B":
+                            resultHexToDec += 11 * (decimal)Math.Pow(16, j);
+                            break;
+                        case "C":
+                            resultHexToDec += 12 * (decimal)Math.Pow(16, j);
+                            break;
+                        case "D":
+                            resultHexToDec += 13 * (decimal)Math.Pow(16, j);
+                            break;
+                        case "E":
+                            resultHexToDec += 14 * (decimal)Math.Pow(16, j);
+                            break;
+                        case "F":
+                            resultHexToDec += 15 * (decimal)Math.Pow(16, j);
+                            break;
+                        default:                            
+                            Console.WriteLine("Character {0} is not from HEXA number. Can not convert input {1}" +
+                                "", consoleInput.Substring(i, 1), consoleInput);
+                            goto Error;                            
+                    }
+                }                
+            }
+            Console.WriteLine(resultHexToDec);
+            Error:
+                Console.WriteLine();
+
+            EndOfScript();
 
             //16.Write a program that by a given integer N prints the numbers from 1 to N 
             //in random order.
-
+            
             //17.Write a program that given two numbers finds their greatest common
             //divisor(GCD) and their least common multiple(LCM).You may use
             //the formula LCM(a, b) = | a * b | / GCD(a, b).
